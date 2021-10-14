dff1f7d9eaf5dc40eda7cf63b344f0d8ec9f2b28
fednerdabady <jenasha29@gmail.com>
2021-10-14 08:21:56 -0300

    commit no index.php e main

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,7 +1,7 @@
 <?php
 
 // uncomment the following to define a path alias
-//Yii::setPathOfAlias('local','path/to/local-folder');
+Yii::setPathOfAlias('local','path/to/local-folder');
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
@@ -73,18 +73,18 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-                /*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-                */
+
+//				array(
+//					'class'=>'CWebLogRoute',
+//				),
+
 
 			),
 		),
 	),
 
 	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
+	//using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
